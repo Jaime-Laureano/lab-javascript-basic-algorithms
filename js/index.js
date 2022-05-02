517fd1d83265dedcb3d5131cc38e54d2154d5b65
Jaime-Laureano <96260804+Jaime-Laureano@users.noreply.github.com>
2022-05-02 11:11:45 +0100

    Updated code for students

--- js/index.js
+++ js/index.js
@@ -1,70 +1,146 @@
 // Iteration 1: Names and Input
 
-let hacker1 = "Jaime"
+let hacker1 = "Bob";
+console.log(`The driver's name is ${hacker1}`);
 
-console.log ("The driver's name is" + hacker1)
-
-let hacker2 = "Amy"
-
-console.log ("The navigator's name is " + hacker2)
+let hacker2 = "Sarah";
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
-if (hacker1.length > hacker2.length){
-    console.log (`The driver has the longest name, it has ${hacker1.length} characters`)
-} else if (hacker2.length > hacker1.length){
-    console.log (`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
-} else {`Wow, you both have equally long names, ${hacker1.length} characters!`}
-
+if (hacker1.length > hacker2.length) {
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters.`
+  );
+} else if (hacker2.length > hacker1.length) {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
+  );
+} else {
+  console.log(
+    `Wow, you both have equally long names, ${hacker1.length} characters!`
+  );
+}
 
 // Iteration 3: Loops
 
-let allCapsDrivers = " "
+// Iteration 3.1
 
-for (let i=0; i<hacker1.length; i++){
-    allCapsDrivers += hacker1[i].toUpperCase() + " "
-};
+let driverCapitals = "";
 
-console.log(allCapsDrivers);
+for (let i = 0; i < hacker1.length; i++) {
+  let upperCaseLetter = hacker1[i].toUpperCase();
 
-let reverseNavigatorName = " "
+  // if it is the first letter to be added, then add it without the space
+  if (i === 0) {
+    driverCapitals = driverCapitals + upperCaseLetter;
+  } else {
+    driverCapitals = driverCapitals + " " + upperCaseLetter;
+  }
+}
 
-for (let i = hacker2.length -1; i>=0; i--){
-    reverseNavigatorName+= hacker2[i]
-};
+console.log("driverCapitals", driverCapitals);
 
-console.log (reverseNavigatorName)
+// Iteration 3.2
 
-if(hacker1===hacker2){
-    console.log ("What?! You both have the same name?")
-} else if (hacker1<hacker2){
-    console.log ("The driver's name goes first.")
+let reversedName = "";
+
+// a reverse loop:
+// starts at the variable length - 1 (the last index),
+// goes while i is higher or equal to 0
+// and decreases the variable i
+for (let i = hacker1.length - 1; i >= 0; i--) {
+  reversedName = reversedName + hacker1[i];
+}
+
+console.log("reversedName", reversedName);
+
+// Iteration 3.3 lexicographic
+
+// first we identify the longest word for the loop
+let longestWord = "";
+if (hacker1.length >= hacker2.length) {
+  longestWord = hacker1;
 } else {
-    console.log ("Yo, the navigator goes first definitely.")
+  longestWord = hacker2;
 }
 
-// Bonus 1
-
-let text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis dictum ullamcorper. Donec a finibus elit. Sed ex est, ullamcorper vitae facilisis in, rhoncus vitae lorem. Vivamus accumsan scelerisque pellentesque. Vivamus lorem tortor, tempor vitae feugiat ut, tempor scelerisque lacus. Aenean id viverra lectus. Nunc condimentum elementum elit vel facilisis. Praesent laoreet eros eget accumsan rhoncus. Mauris sit amet cursus tellus. Quisque tempus, nisi a congue vehicula, lacus dui dignissim mauris, a posuere tellus nisi at magna. Quisque sit amet mollis risus. Maecenas in magna gravida, varius erat sed, lacinia magna. Quisque pretium nibh et libero pharetra sollicitudin. In hac habitasse platea dictumst. Duis neque diam, blandit at tristique nec, consectetur et sapien. Integer non nunc at purus auctor ultricies.
-Praesent consequat efficitur mi vel tempus. Curabitur sit amet nibh feugiat, scelerisque arcu quis, sagittis eros. Fusce dignissim posuere mi, sit amet pulvinar magna vestibulum ac. Aenean eget turpis tortor. In hac habitasse platea dictumst. Fusce libero sapien, varius et tortor quis, tristique fermentum erat. Suspendisse maximus laoreet ligula. Curabitur viverra ac urna id bibendum. Vestibulum at lorem a augue ultrices pretium sit amet a lacus. Nam luctus semper sagittis. Etiam diam felis, sagittis vitae leo ut, porttitor tincidunt est. Praesent convallis commodo fringilla. Suspendisse congue pulvinar nisi, at aliquam mauris vestibulum ac. Maecenas maximus, tellus in interdum faucibus, nibh odio posuere odio, a varius magna enim non mi. Sed eu justo sit amet turpis sagittis hendrerit. Proin nunc sem, ultrices vitae venenatis in, placerat quis dolor.
-Nullam pretium euismod ante, sed vestibulum quam tincidunt at. Donec et varius ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam rhoncus elit magna, ac placerat sapien tempor non. Maecenas suscipit malesuada velit, ac pretium neque efficitur ac. Nunc maximus pellentesque cursus. Quisque vitae arcu libero. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis at euismod eros. Donec eros lectus, facilisis quis nulla in, hendrerit ultricies lorem. Cras nec pulvinar leo. Donec convallis auctor suscipit.`
-
-
-let etCount = 0
-
-for (let i=0; i<wordsInText.length; i++) {
-    if (text[i]=== 'et'|| text[i] === 'Et') {
-        etCount++ 
-    }
+// the loop runs for every character and compares them.
+for (let i = 0; i < longestWord.length; i++) {
+  if (hacker1 === hacker2) {
+    // first condition, in case they actually have the same name
+    console.log("What?! You both have the same name?");
+    break;
+  } else if (hacker1[i] < hacker2[i] || hacker1[i] === undefined) {
+    // second condition will check if the letter of the driver is lower (goes first) or doesn't exist.
+    console.log(`The driver ${hacker1} goes first.`);
+    break;
+  } else if (hacker2[i] < hacker1[i] || hacker2[i] === undefined) {
+    // third condition will check if the letter of the navigator is lower (goes first) or doesn't exist.
+    console.log(`Yo, the navigator ${hacker2} goes first definitely.`);
+    break;
+  }
+  // loop will continue running for each character until it found one match, then it breaks the loop.
 }
 
-console.log (etCount)
+// Bonus 1: Iorem Ipsum
 
-let wordCount = text.split(" ")
+let ipsumStr =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet est at justo tempor mollis. Aliquam consequat lacinia interdum. Fusce sodales dui a elit pulvinar ornare. Integer ullamcorper nunc vel finibus venenatis. Sed lectus velit, lacinia non posuere a, porta fringilla turpis. Cras sagittis erat malesuada risus posuere fringilla. Etiam vulputate dolor metus, in pharetra velit dictum non. Vivamus eu augue nec est tempus viverra. Phasellus eu augue volutpat, fermentum risus et, dapibus purus. Quisque vel augue mauris. Maecenas mattis ipsum sit amet metus dictum, quis pharetra enim cursus. Sed ac ullamcorper turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. In consectetur diam ac turpis malesuada rutrum. Sed porta nulla id enim vulputate, eu tristique tortor cursus.";
 
-console.log (wordCount.length)
+// to count all words
 
+let wordCount = 1;
 
+for (let i = 0; i < ipsumStr.length; i++) {
+  // counting the spaces means counting the words!
+  if (ipsumStr[i] === " ") {
+    wordCount++;
+  }
+}
 
-// Bonus 2
+console.log("Amount of words: " + wordCount);
 
+// to count only "et" words
+
+let etCount = 0;
+
+for (let i = 0; i < ipsumStr.length; i++) {
+  // counts if current letter is "e" and the next letter is "t"
+  if (ipsumStr[i] === "e" && ipsumStr[i + 1] === "t") {
+    etCount++;
+  }
+}
+
+console.log("Amount of et: " + etCount);
+
+// Bonus 2: Palindrome
+
+let phraseToCheck = "tacocat";
+
+// first we create a clean variable with all special characters removed.
+let cleanPhrase = "";
+for (let i = 0; i < phraseToCheck.length; i++) {
+  // only add characters between "A" and "z"
+  if (phraseToCheck[i] >= "A" && phraseToCheck[i] <= "z") {
+    cleanPhrase = cleanPhrase + phraseToCheck[i];
+  }
+}
+console.log("cleanPhrase", cleanPhrase);
+
+// then we reverse the phrases
+let reversedPhrase = "";
+
+// to do that, we do a reverse loop and add the characters to a new string
+for (let i = cleanPhrase.length - 1; i >= 0; i--) {
+  reversedPhrase = reversedPhrase + cleanPhrase[i];
+}
+
+console.log("reversedPhrase", reversedPhrase);
+
+// then simply compare the two variables to check for a palindrome!
+if (cleanPhrase === reversedPhrase) {
+  console.log("The phrase is a palindrome!");
+} else {
+  console.log("The phrase is not a palindrome");
+}
